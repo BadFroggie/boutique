5ed9eec9159896b050f3331c918721f47d8fbf14
BadFroggie <68106114+BadFroggie@users.noreply.github.com>
2020-07-16 16:25:11 +0200

    Add files via upload

--- src/Chaussures.php
+++ src/Chaussures.php
@@ -2,11 +2,14 @@
 
 class Chaussures extends Vetements
 {
+    public $id;
+    public $nom;
     public $pointure;
 
-    public function __construct($prix_instance, $dispo_instance, $point_instance)
+    public function __construct($prix_instance, $dispo_instance, $nom_instance, $point_instance)
     {
         parent::__construct($prix_instance, $dispo_instance);
+        $this->nom = $nom_instance;
         $this->pointure = $point_instance;
     }
 }
--- src/Pantalons.php
+++ src/Pantalons.php
@@ -2,11 +2,14 @@
 
 class Pantalons extends Vetements
 {
+    public $id;
+    public $nom;
     public $taille;
 
-    public function __construct($prix_instance, $dispo_instance, $taille_instance)
+    public function __construct($prix_instance, $dispo_instance, $nom_instance, $taille_instance)
     {
         parent::__construct($prix_instance, $dispo_instance);
+        $this->nom = $nom_instance;
         $this->taille = $taille_instance;
     }
 }
